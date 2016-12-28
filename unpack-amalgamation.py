43e78bce25e872710a4e97e9fc5e90d194dc20b7
buaa.byl <buaa.byl@gmail.com>
2016-12-28 11:56:39 +0800

    Support regenerate MD5 hashvalue of amalgamation files.

--- unpack-amalgamation.py
+++ unpack-amalgamation.py
@@ -1,4 +1,4 @@
-#!/bin/python
+#!/usr/bin/env python2
 # -*- coding:utf-8 -*-
 #  
 #  Copyright 2013 buaa.byl@gmail.com
@@ -18,13 +18,29 @@
 #  along with this program; see the file COPYING.  If not, write to
 #  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 #
+from __future__ import print_function
+import copy
 import sys
 import os
 import re
 from hashlib import md5
+from hashlib import sha1
+from hashlib import sha256
+from hashlib import sha512
 
 regex_file = re.compile(r'^[ *]+File: ([0-9a-zA-Z_.-]+)\W*$')
-regex_md5  = re.compile(r'^[ *]+MD5: ([0-9a-fA-F]+)\W*$')
+regex_md5  = re.compile(r'^[ *]+(MD5): ([0-9a-fA-F]+)\W*$')
+regex_id   = re.compile(r'^[ *]+(ID): ([0-9a-fA-F]+)\W*$')
+
+HEADER = '''\
+/*
+ * ----------------------------------------------------------
+ * File: %(filename)s
+ * ID: %(hash)s
+ * ----------------------------------------------------------
+ */\
+'''
+
 
 def file_get_contents(fn):
     f = open(fn, 'r')
@@ -37,69 +53,117 @@ def file_put_contents(fn, d):
     f.write(d)
     f.close()
 
-if __name__ == '__main__':
-    target_dir = sys.argv[2]
-    v = file_get_contents(sys.argv[1])
+def new_info(INFO, lineno, filename, newtype, newhash):
+    info = copy.deepcopy(INFO)
+    info['lineno']  = lineno + 1
+    info['filename']= filename
+    info['type']    = newtype
+    info['oldhash'] = newhash
+    return info
 
-    lines = v.split('\n')
+def build_context(info, lines):
+    text = '\n'.join(lines)
+    hashstr = md5(text).hexdigest()
+    info['hash']    = hashstr
+    info['range']   = (info['lineno'], info['lineno'] + len(lines) - 1)
+    info['text']    = text
+    return info
+
+if __name__ == '__main__':
+    source_fn   = sys.argv[1]
+    v = file_get_contents(source_fn)
+
+    print('parsing...', source_fn)
+    print()
+
+    INFO = {
+        'lineno'    :0,
+        'range'     :(0, ),
+        'filename'  :'',
+        'type'      :'',
+        'oldhash'   :'',
+        'hash'      :'',
+        'text'      :''
+    }
+
+    lines = v.splitlines()
     nr_lines = len(lines)
 
-    fn = 'amalgamation.h'
-    md5_value = ''
-    lst_line = []
+    lst_unpacked = []
 
-    print fn
+    lineno = 0
+    file_context = []
 
-    i = 0
-    while i < nr_lines:
-        line = lines[i]
+    info = new_info(INFO, lineno, 'amalgamation.h', '', '')
+
+    while lineno < nr_lines:
+        line = lines[lineno]
+
+        if line.find('END-OF-IMPLEMENTATION:') >= 0:
+            if len(file_context) > 0:
+                lst_unpacked.append(build_context(info, file_context))
+                info = new_info(INFO, lineno, 'amalgamation.c', newtype, newhash)
+            file_context = lines[lineno:]
+            break
 
         res = regex_file.match(line)
         if res:
-            fn_new = res.groups()[0]
+            fn_new  = res.groups()[0]
+            line    = lines[lineno + 1]
+            res     = regex_md5.match(line)
+            if not res:
+                res = regex_id.match(line)
+            if not res:
+                file_context.append(lines[lineno])
+                lineno += 1
+                continue
 
-            line = lines[i + 1]
-            res = regex_md5.match(line)
-            if res:
-                md5_value = res.groups()[0]
-            else:
-                print 'error'
-                break
+            newtype = res.groups()[0]
+            newhash = res.groups()[1]
+            lineno += 4
 
-            i += 4
+            file_context.pop(-1)
+            file_context.pop(-1)
 
-            lst_line.pop(-1)
-            lst_line.pop(-1)
-
-            if len(lst_line) > 0:
-                text = '\n'.join(lst_line)
-                file_put_contents(os.path.join(target_dir, fn), text)
-                print '%s (wrote)' % (md5(text).hexdigest())
-
-                del lst_line
-                del text
-                fn = fn_new
-                lst_line = []
-
-            print
-            print fn_new
-            print md5_value
-
+            if len(file_context) > 0:
+                lst_unpacked.append(build_context(info, file_context))
+                info = new_info(INFO, lineno, fn_new, newtype, newhash)
+                file_context = []
             continue
 
-        lst_line.append(line)
+        file_context.append(line)
+        lineno += 1
 
-        i += 1
-        pass
+    if len(file_context) > 0:
+        lst_unpacked.append(build_context(info, file_context))
 
-    if len(lst_line) > 0:
-        text = '\n'.join(lst_line)
-        file_put_contents(os.path.join(target_dir, fn), text)
-        print '%s (wrote)' % (md5(text).hexdigest())
-        del lst_line
-        del text
-        fn = fn_new
-        lst_line = []
+    for db in lst_unpacked:
+        print('%s:%d %d-%d' % (sys.argv[1], db['lineno'], db['range'][0], db['range'][1]))
+        print('File: %s' % db['filename'])
+        print('MD5 : %s %s parsed' % (db['type'], db['oldhash']))
+        print('MD5 : %s %s calculated' % (db['type'], db['hash']))
+        print()
+
+    if len(sys.argv) == 3:
+        target_dir  = sys.argv[2]
+        for db in lst_unpacked:
+            fn = os.path.join(target_dir, db['filename'])
+            file_put_contents(fn, db['text'])
+            print('wrote %s' % fn)
+        print()
+
+    l = []
+    for db in lst_unpacked:
+        if db['filename'].startswith('amalgamation'):
+            l.append(db['text'])
+        else:
+            l.append(HEADER % db)
+            l.append(db['text'])
+    l.append('')
+
+    fn = source_fn + '.__regenerate__.c'
+    print('wrote %s' % fn)
+    file_put_contents(fn, '\n'.join(l))
 
 
 
